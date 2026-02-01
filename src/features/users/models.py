a6469f053ecd5d8629f0196117591bb00ba3df06
Wildann77 <boyblanco77@gmail.com>
2026-02-01 14:26:12 +0700

    add userModel.py

---
+++ src/features/users/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime, func, Boolean
+from src.core.base import Base, SoftDeleteMixin
+
+class UserModel(Base, SoftDeleteMixin):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
